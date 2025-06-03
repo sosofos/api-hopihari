e8dc391aaeaef5cfe9859ee2a3317a792dbe9371
sosofos <dantassofia84@gmail.com>
2025-06-03 09:22:08 -0300

    dia 0306

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -7,15 +7,18 @@ exports.atualizarUsuario = async (req, res) => {
         const idUsuario = Number(req.params.id);
         const resultado = await mysql.execute(
             `UPDATE users
-                SET name     = ?,
-                    email    = ?,
-                    password = ?
+                SET first_name     = ?,
+                last_name =?,
+                phone=?,    
+               birth_date=?,
+               email=?,
               WHERE id = ?;`,
             [
-                req.body.name,
+                req.body.first_name,
+                req.body.last_name
+                req.body.phone,
                 req.body.email,
-                req.body.password,
-                idUsuario
+               res.locals.idUsuario
             ]
         );
         return res.status(201).send({
--- routes/usuarios.route.js
+++ routes/usuarios.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const usuariosController = require("../controllers/usuarios.controller");
+const login = require("../middleware/usuarios.middleware")
 
 router.post('/login', usuariosController.login);
-router.put('/:id', usuariosController.atualizarUsuario);
+router.put('/', login.required, usuariosController.atualizarUsuario);
 router.post('/', usuariosController.cadastrarUsuario);
 
 module.exports = router;
